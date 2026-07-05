5179a82fa3b5e4e523bf68ad73aeff84ef1e7ad5
amajewski1994 <amajewski1994@gmail.com>
2026-07-05 10:08:04 +0200

    update package.json and next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/agent-forge",
+  assetPrefix: "/agent-forge/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
